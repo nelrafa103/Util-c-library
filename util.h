fd5e5f778b3a460988960e3ec4ec6f101ccfe0a9
nelrafa103 <nelcidodiaz13@gmail.com>
2021-04-26 18:05:13 -0400

    Iniatilizig the proyect:  - Added a new file called util.h    -Added three new functions:     + getch(void)     + gotoxy(int,int)     + color(char *)

---
+++ util.h
@@ -0,0 +1,24 @@
+void gotoxy(int x, int y)
+{
+    printf("%c[%d;%df", 0x1B, y, x);
+}
+
+
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+
+void color()
+{
+
+}
